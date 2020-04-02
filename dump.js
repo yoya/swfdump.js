62328a338b2e80555b592338b75078ac802a0769
Yoshihiro Yamazaki <yoya@awm.jp>
2020-04-03 00:07:19 +0900

    view tags.

--- dump.js
+++ dump.js
@@ -9,12 +9,16 @@ function main() {
 }
 
 function tableEntry(table, key, value) {
-    const thead = table.children[0],
-          tbody = table.children[1];
+    let thead = table.children[0];
+    let tbody  = table.children[1];
+    if (table.children.length > 2) { // with caption
+        thead = table.children[1];
+        tbody = table.children[2];
+    }
     const thead_tr = thead.children[0],
           tbody_tr = tbody.children[0];
     const th = document.createElement("th"),
-          td = document.createElement("td")
+          td = document.createElement("td");
     th.innerHTML = key;
     td.innerHTML = value;
     thead_tr.append(th);
@@ -28,6 +32,7 @@ function swfdump(arr) {
     display.innerHTML = "";  // clear all elements in display
     const swfheader = document.getElementById("swfheader").cloneNode(true);
     swfheader.id = "";
+    swfheader.style = "float:left";
     display.append(swfheader);
     let sig     = bit.string(3),
         ver     = bit.u8(),
@@ -37,11 +42,34 @@ function swfdump(arr) {
     tableEntry(swfheader, "FileLength", filelen);
     const swfmovieheader = document.getElementById("swfmovieheader").cloneNode(true);
     swfmovieheader.id = "";
+    // swfmovieheader.style = "float:left";
     display.append(swfmovieheader);
-    const [xMin, xMax, yMin, yMax] = SWFRect(bit);
+    const rect = new SWFRect(bit);
     const rate  = bit.u16(),
           count = bit.u16();
-    tableEntry(swfmovieheader, "FrameSize" , "x:"+xMin+"..."+xMax+", y:"+yMin+"..."+yMax);
+    tableEntry(swfmovieheader, "FrameSize" , rect.string());
     tableEntry(swfmovieheader, "FrameRate" , rate / 0x100);
     tableEntry(swfmovieheader, "FrameCount", count);
+    swfdump_tags(display, bit, 0);
+}
+
+function swfdump_tags(display, bit, count) {
+    let prevCode = null;
+    while (bit.has(2)) {
+        let [code, length] = SWFTagHeader(bit);
+        let tagType = SWFTagType(code);
+        let swftag_id = ["swfunknowntag","swfdefinitiontag", "swfoveralltag",
+                         "swfplaylisttag", "swfactiontag" ] [tagType];
+        const swftag = document.getElementById(swftag_id).cloneNode(true);
+        swftag.className = swftag.id;
+        swftag.id = "";
+        if (code !== 1) {
+            swftag.style = "float:left";
+        }
+        display.append(swftag);
+        tableEntry(swftag, "Code", SWFTagName(code)+"("+code+")");
+        tableEntry(swftag, "Length", length);
+        bit.seek(bit.offset() + length);
+        prevCode = code;
+    }
 }
--- swf.js
+++ swf.js
@@ -2,11 +2,179 @@
 
 const TWIPS = 20;
 
+const SWFUnknownTag         = 0;
+const SWFDefinitionTag      = 1;
+const SWFOverallControlTag  = 2;
+const SWFPlayListControlTag = 3;
+const SWFActionControlTag   = 4;
+
+const SWFDefinitionTags = [
+    2/*DefineShape*/, 6/*DefineBits*/, 7/*DefineButton*/,
+    8/*JEGTables*/, 10/*DefineFont*/, 11/*DefineText*/, 13/*DefineFontInfo*/,
+    14/*DefineSound*/, 17/*DefineButtonSound*/,
+    18/*SoundStreamHead*/, 19/*SoundStreamBlock*/,
+    20/*DefineBitsLossless*/,  21/*DefineBitsJPEG2*/,
+    22/*DefineShape2*/, 23/*DefineButtonCxform*/, 32/*DefineShape3*/,
+    33/*DefineText2*/, 34/*DefineButton2*/,
+    35/*DefineBitsJPEG3*/, 36/*DefineBitsLossless2*/,
+    37/*DefineEditText*/, 39/*DefineSprite*/, 45/*SoundStreamHead2*/,
+    46/*DefineMorphShape*/, 48/*DefineFont2*/,
+    60/*DefineVideoStream*/, 61/*VideoFrame*/,
+    62/*DefineFontInfo2*/, 73/*DefineFontAlignZones*/, 74/*CSMTextSettings*/,
+    75/*DefineFont3*/, 78/*DefineScalingGrid*/,
+    83/*DefineShape4*/, 84/*DefineMorphShape2*/,
+    86/*DefineSceneAndFrameLabelData*/, 87/*DefineBinaryData*/,
+    88/*DefineFontName*/, 88/*DefineFontName*/,
+    90/*DefineBitsJPEG4*/, 91/*DefineFont4*/,
+];
+
+const SWFOverallControlTags = [
+    0/*End*/,
+    9/*SetBackgroundColor*/,
+    24/*Protect*/, 43/*FrameLabel*/,
+    56/*ExportAsset*/, 57/*ImportAssets*/, 58/*EnableDebugger*/,
+    64/*EnableDebugger2*/, 65/*ScriptLimits*/, 66/*SetTabIndex*/,
+    69/*FileAttributes*/, 71/*ImportAssets2*/, 77/*MetaData*/,
+];
+
+const SWFPlayListControlTags = [
+    1/*ShowFrame*/, 4/*PlaceObject*/, 5/*RemoveObject*/, 15/*StartSound*/,
+    26/*PlaceObject2*/, 28/*RemoveObject2*/, 70/*PlaceObject3*/,
+    89/*StartSound2*/
+];
+
+const SWFActionControlTags = [
+    12/*DoAction*/, 59/*DoInitAction*/,
+    76/*SymbolClass*/, 82/*DoABC*/
+];
+
+const SWFTags = {
+    // id => name, function
+    0: ["End"],
+    1: ["ShowFrame"],
+    2: ["DefineShape"],
+    3: ["FreeCharacter"], // 3 missing private use
+    4: ["PlaceObject"],
+    5: ["RemoveObject"],
+    6: ["DefineBits"],
+    7: ["DefineButton"],
+    8: ["JEGTables"],
+    9: ["SetBackgroundColor"],
+    10: ["DefineFont"],
+    11: ["DefineText"],
+    12: ["DoAction"],
+    13: ["DefineFontInfo"],
+    14: ["DefineSound"],
+    15: ["StartSound"],
+    // 16 missing
+    17: ["DefineButtonSound"],
+    18: ["SoundStreamHead"],
+    19: ["SoundStreamBlock"],
+    20: ["DefineBitsLossless"],
+    21: ["DefineBitsJPEG2"],
+    22: ["DefineShape2"],
+    23: ["DefineButtonCxform"],
+    24: ["Protect"],
+    // 25 missing
+    26: ["PlaceObject2"],
+    // 27 missing
+    28: ["RemoveObject2"],
+    // 29,30,31 missing
+    32: ["DefineShape3"],
+    33: ["DefineText2"],
+    34: ["DefineButton2"],
+    35: ["DefineBitsJPEG3"],
+    36: ["DefineBitsLossless2"],
+    37: ["DefineEditText"],
+    // 38 miggin
+    39: ["DefineSprite"],
+    // 40,41,42 missing
+    43: ["FrameLabel"],
+    // 44 missing
+    45: ["SoundStreamHead2"],
+    46: ["DefineMorphShape"],
+    48: ["DefineFont2"],
+    56: ["ExportAssets"],
+    57: ["ImportAssets"],
+    58: ["EnableDebugger"],
+    59: ["DoInitAction"],
+    60: ["DefineVideoStream"],
+    61: ["VideoFrame"],
+    62: ["DefineFontInfo2"],
+    // 63 missing
+    64: ["EnableDebugger2"],
+    65: ["ScriptLimits"],
+    66: ["SetTabIndex"],
+    // 67,68 missing
+    69: ["FileAttributes"],
+    70: ["PlaceObject3"],
+    71: ["ImportAssets2"],
+    // 72 missing
+    73: ["DefineFontAlignZones"],
+    74: ["CSMTextSettings"],
+    75: ["DefineFont3"],
+    76: ["SymbolClass"],
+    77: ["MetaData"],
+    78: ["DefineScalingGrid"],
+    // 79,80,81 missing
+    82: ["DoABC"],
+    83: ["DefineShape4"],
+    84: ["DefineMorphShape2"],
+    // 85 missing
+    86: ["DefineSceneAndFrameLabelData"],
+    87: ["DefineBinaryData"],
+    88: ["DefineFontName"],
+    88: ["DefineFontName"],
+    89: ["StartSound2"],
+    90: ["DefineBitsJPEG4"],
+    91: ["DefineFont4"],
+    777: ["Reflex"], // swftools
+}
+
+function SWFTagName(id) {
+    return SWFTags[id][0];
+}
+
+function SWFTagType(code) {
+    if (SWFDefinitionTags.indexOf(code) >= 0) {
+        return SWFDefinitionTag;
+    } else if (SWFOverallControlTags.indexOf(code) >= 0) {
+        return SWFOverallControlTag;
+    } else if (SWFPlayListControlTags.indexOf(code) >= 0) {
+        return SWFPlayListControlTag;
+    } else if (SWFActionControlTags.indexOf(code) >= 0) {
+        return SWFActionControlTag;
+    }
+    return SWFUnknownTag;
+}
+
+/*
+  type
+*/
 function SWFRect(bit) {
     const rectBit = bit.ub(5),
           xMin = bit.sb(rectBit) / TWIPS,
           xMax = bit.sb(rectBit) / TWIPS,
           yMin = bit.sb(rectBit) / TWIPS,
           yMax = bit.sb(rectBit) / TWIPS;
-    return [xMin, xMax, yMin, yMax];
+    this.__proto__ = {
+        string: function() {
+            return "X:"+xMin+"..."+xMax+", Y:"+yMin+"..."+yMax;
+        }
+    }
+    return this;
+}
+
+/*
+  basic structure
+*/
+
+function SWFTagHeader(bit) {
+    const tag_and_length = bit.u16();
+    const tag = tag_and_length >> 6;
+    let length = tag_and_length & 0x3f;
+    if (length == 0x3f) {
+        length = bit.u32();
+    }
+    return [tag, length];
 }
